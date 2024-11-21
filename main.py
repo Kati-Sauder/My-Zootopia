443b78e233129b37c6bef9b6540d4f425ec27c8a
Kati-Sauder <katarina.aguilerasauder@gmail.com>
2024-11-21 13:11:24 +0100

    access data from json file

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("huhu")
+import json
+
+
+def load_data (file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+animals_data = load_data('animals_data.json')
+
+
+animals_info = []
+for info in animals_data:
+    name = info.get("name")
+    diet = info.get("characteristics", {}).get("diet")
+    location = info.get("locations")[0]
+    fox_type = info.get("characteristics", {}).get("type")
+
+    animals_info.append({
+        "name": name,
+        "diet": diet,
+        "location": location,
+        "type": fox_type
+    })
+
+for animal in animals_info:
+    print(f"Name: {animal['name']}")
+    print(f"Diet: {animal['diet']}")
+    print(f"Location: {animal['location']}")
+
+    if animal['type'] is not None:
+        print(f"Type: {animal['type']}")
+
+    print()
+
+
+
+
+
